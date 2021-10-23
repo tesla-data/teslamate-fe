22e067323841e816e36fe628c1e18e6d2a0b2812
Tal <talrasha007@gmail.com>
2021-10-23 22:17:58 +0800

    router

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Home from './views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes
+})
