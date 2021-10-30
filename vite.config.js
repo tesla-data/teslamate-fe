05bfb1909e252d89f085f35c20daa4181d300413
Tal <talrasha007@gmail.com>
2021-10-30 22:56:33 +0800

    nutui basic

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: '@nutui/nutui',
+          libraryNameChangeCase: 'pascalCase',
+          resolveStyle: (name) => {
+            return `@nutui/nutui/dist/packages/${name}/index.scss`
+          }
+        }
+      ]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 配置 nutui 全局 scss 变量
+        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
+      }
+    }
+  }
 })
