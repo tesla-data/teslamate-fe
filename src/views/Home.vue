991276f03a14a4db05f53b34b93a8d6d0373905b
Tal <talrasha007@gmail.com>
2021-11-01 02:44:10 +0800

    settings link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
 <div v-if="currentVehicle" class="vehice-state">
-  <h4>{{currentVehicle.name}}</h4>
+  <div style="display: flex;">
+    <h4>{{currentVehicle.name}}</h4>
+    <div style="flex-grow: 1;" />
+    <router-link to="settings" style="color: #333;"><nut-icon size="18" name="setting" /></router-link>
+  </div>
   <div v-if="state" style="display: flex; align-items: center;">
     <battery style="margin-right: 8px;" :percent="state.battery_level" />
     <div>
@@ -23,7 +27,8 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 import { Cell, CellGroup } from '@nutui/nutui'
 
 import Battery from '../components/Battery.vue'
@@ -32,6 +37,12 @@ import { currentVehicle } from '../api/vehicles'
 import { km } from '../filters'
 
 const state = ref(null)
+
+onMounted(async () => {
+  if (currentVehicle.value) {
+    state.value = await vehicle.getState()
+  }
+})
 
 watch(currentVehicle, async ({ id: carId }) => {
   if (carId) {
