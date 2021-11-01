691b0b1b590601ddb09a783070e2b30e0d4ae3fd
Tal <talrasha007@gmail.com>
2021-11-01 16:13:14 +0800

    drives

--- src/views/Drives.vue
+++ src/views/Drives.vue
@@ -1,12 +1,12 @@
 <template>
-<navbar @on-click-back="$router.go(-1)" title="统计" />
+<navbar @on-click-back="$router.go(-1)" title="行程" />
 <div class="page" style="margin-top: -40px;">
-  {{drives[0]}}
+  <!-- {{drives[0]}} -->
 
-  <cell-group title="统计结果">
+  <cell-group title="行程">
     <cell
       v-for="d of drives" :title="d.start_address"
-      :desc="`${d.duration_min}分钟 | ${d.distance_km}`"
+      :desc="`${d.duration_min}分钟 | ${d.distance_km.toFixed(0)}km`"
       :sub-title="`能耗${d.consumption_kwh_km && d.consumption_kwh_km.toFixed(0)}Wh/km`"
       is-link
     />
