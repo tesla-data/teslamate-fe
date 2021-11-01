6210e48bfa00f045164661e4a115abbd3369fcee
Tal <talrasha007@gmail.com>
2021-11-01 12:36:05 +0800

    style

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -4,8 +4,8 @@
   <cell-group title="统计结果">
     <cell
       v-for="s of stats" :title="s.display"
-      :sub-title="`行驶了${(s.sum_duration_h / 60).toFixed(0)}分钟,距离${s.sum_distance_km}km,`"
-      :desc="`平均气温${s.avg_outside_temp_c.toFixed(1)}℃，能耗${(145 / s.efficiency).toFixed(0)}Wh/km`"
+      :desc="`${(s.sum_duration_h / 60).toFixed(0)}分钟 | ${s.sum_distance_km}km,`"
+      :sub-title="`气温${s.avg_outside_temp_c.toFixed(1)}℃ 能耗${(145 / s.efficiency).toFixed(0)}Wh/km`"
       is-link
     />
   </cell-group>
