2585a6d03c0bf11f60620af2f4b04f7f34773255
Tal <talrasha007@gmail.com>
2021-11-04 19:17:03 +0800

    temp

--- src/views/Drives.vue
+++ src/views/Drives.vue
@@ -7,7 +7,7 @@
     <cell
       v-for="d of drives" :title="d.start_address"
       :desc="`${d.duration_min}分钟 | ${d.distance_km.toFixed(0)}km`"
-      :sub-title="`能耗${d.consumption_kwh_km && d.consumption_kwh_km.toFixed(0)}Wh/km`"
+      :sub-title="`温度${d.outside_temp_c}℃ 能耗${d.consumption_kwh_km && d.consumption_kwh_km.toFixed(0)}Wh/km`"
       is-link
     />
   </cell-group>
