e40c3aa905c5e2fe1c01249dc73b6f3a852da075
Tal <talrasha007@gmail.com>
2021-11-05 15:52:45 +0800

    update chart

--- src/components/StateHistory.vue
+++ src/components/StateHistory.vue
@@ -5,9 +5,11 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue'
+import { ref, watch, defineProps } from 'vue'
 
 import Highcharts from 'highcharts'
+Highcharts.setOptions({ time: { timezoneOffset: new Date().getTimezoneOffset() }, credits: { enabled: false } })
+
 import xrange from 'highcharts/modules/xrange'
 xrange(Highcharts)
 
@@ -17,22 +19,62 @@ import api from '../api/vehicle'
 const stateChart = ref(null)
 const rangeChart = ref(null)
 const props = defineProps({ history: { type: Array, required: true } })
-console.log(props.history)
 
-api.getStateHistory().then(([stateHistory, rangeHistory]) => {
-  Highcharts.setOptions({ time: { timezoneOffset: new Date().getTimezoneOffset() }, credits: { enabled: false } })
+const stateDesc = [
+  { state: '在线', color: '#86cede' },
+  { state: '行驶', color: '#8445b2' },
+  { state: '充电', color: '#edcc47' },
+  { state: '离线', color: '#f4b567' },
+  { state: '休眠', color: '#6aa356' },
+  { state: '在线', color: '#86cede' },
+  { state: '升级', color: 'red' }
+]
 
-  const stateDesc = [
-    { state: '在线', color: '#86cede' },
-    { state: '行驶', color: '#8445b2' },
-    { state: '充电', color: '#edcc47' },
-    { state: '离线', color: '#f4b567' },
-    { state: '休眠', color: '#6aa356' },
-    { state: '在线', color: '#86cede' },
-    { state: '升级', color: 'red' }
-  ]
+function convertStateHistory(stateHistory) {
+  return {
+    // borderColor: 'gray',
+    pointWidth: 40,
+    data: stateHistory.map(({ time, state }, i) => ({
+      x: time,
+      x2: (stateHistory[i + 1] || { time: Date.now() }).time,
+      y: 0,
+      ...stateDesc[state]
+    })),
+    dataLabels: {
+      enabled: true
+    }
+  }
+}
 
-  Highcharts.chart(stateChart.value, {
+function convertRangeHistory(rangeHistory) {
+  return [{
+    name: '电量',
+    type: 'line',
+    yAxis: 0,
+    lineWidth: 1,
+    connectNulls: true,
+    tooltip: { valueSuffix: '%' },
+    data: rangeHistory.map(({ time, battery_level }) => [time, battery_level])
+  }, {
+    name: '里程',
+    type: 'line',
+    yAxis: 1,
+    lineWidth: 1,
+    connectNulls: true,
+    tooltip: { valueSuffix: 'km' },
+    data: rangeHistory.map(({ time, range }) => [time, range])
+  }]
+}
+
+watch(() => props.history, ([stateHistory, rangeHistory]) => {
+  stateChart.value.chart.series[0].setData(convertStateHistory(stateHistory).data)
+  convertRangeHistory(rangeHistory).forEach((series, i) => {
+    rangeChart.value.chart.series[i].setData(series.data)
+  })
+})
+
+watch(stateChart, () => {
+  stateChart.value.chart = Highcharts.chart(stateChart.value, {
     chart: {
       type: 'xrange'
     },
@@ -74,22 +116,10 @@ api.getStateHistory().then(([stateHistory, rangeHistory]) => {
       }
     },
 
-    series: [{
-      // borderColor: 'gray',
-      pointWidth: 40,
-      data: stateHistory.map(({ time, state }, i) => ({
-        x: time,
-        x2: (stateHistory[i + 1] || { time: Date.now() }).time,
-        y: 0,
-        ...stateDesc[state]
-      })),
-      dataLabels: {
-        enabled: true
-      }
-    }]
+    series: [convertStateHistory(props.history[0])]
   })
 
-  Highcharts.chart(rangeChart.value, {
+  rangeChart.value.chart = Highcharts.chart(rangeChart.value, {
     title: {
       text: null
     },
@@ -114,23 +144,11 @@ api.getStateHistory().then(([stateHistory, rangeHistory]) => {
         enabled: false
     },
 
-    series: [{
-      name: '电量',
-      type: 'line',
-      yAxis: 0,
-      lineWidth: 1,
-      connectNulls: true,
-      tooltip: { valueSuffix: '%' },
-      data: rangeHistory.map(({ time, battery_level }) => [time, battery_level])
-    }, {
-      name: '里程',
-      type: 'line',
-      yAxis: 1,
-      lineWidth: 1,
-      connectNulls: true,
-      tooltip: { valueSuffix: 'km' },
-      data: rangeHistory.map(({ time, range }) => [time, range])
-    }]
+    series: convertRangeHistory(props.history[1])
   })
 })
+
+// api.getStateHistory().then(([stateHistory, rangeHistory]) => {
+
+// })
 </script>
