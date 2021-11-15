92c7d0a6810ae24234bebc2ea88c4e79807629b2
Tal <talrasha007@gmail.com>
2021-11-15 13:03:42 +0800

    update used days

--- src/filters.js
+++ src/filters.js
@@ -6,6 +6,15 @@ export function duration(d) {
     Math.floor(d / 86400) + '天' + Math.round((d % 86400) / 3600) + '小时'
 }
 
+export function days(d) {
+  d = Math.round(d / 1000)
+  return d < 60 ? d + '秒' :
+    d < 3600 ? Math.round(d / 60) + '分' :
+    d < 86400 ? Math.round(d / 3600) + '小时' :
+    Math.round(d / 86400) + '天'
+}
+
+
 export function km(v, digits = 0) {
   return v && ((v * 1).toFixed(digits) + 'km')
 }
--- src/views/Updates.vue
+++ src/views/Updates.vue
@@ -3,9 +3,9 @@
 <div class="page">
   <cell-group title="统计结果">
     <cell
-      v-for="u of updates" :title="u.version"
+      v-for="(u, i) of updates" :title="u.version"
       :desc="`平均续航${(u.avg_range || 0).toFixed(0)}km`"
-      :sub-title="`${new Date(u.time).toLocaleDateString()} 充电${u.chg_ct}次`"
+      :sub-title="`${new Date(u.time).toLocaleDateString()} 使用${days(i > 0 ? updates[i - 1].since_last_update * 1000 : Date.now() - u.time )} 充电${u.chg_ct}次`"
     />
   </cell-group>
 </div>
@@ -14,6 +14,7 @@
 <script setup>
 import { ref } from 'vue'
 import { Navbar, CellGroup, Cell } from '@nutui/nutui'
+import { days } from '../filters'
 
 import api from '../api/vehicle'
 
