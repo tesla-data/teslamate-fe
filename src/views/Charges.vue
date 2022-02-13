5250ed467a1cc8bd2d7bc6059fd11daf7048bb3d
Tal <talrasha007@gmail.com>
2022-02-13 16:30:05 +0800

    charge stats group

--- src/views/Charges.vue
+++ src/views/Charges.vue
@@ -1,12 +1,12 @@
 <template>
 <navbar @on-click-back="$router.go(-1)" title="充电" class="navbar" />
 <div class="page">
-  <cell-group title="统计结果">
+  <cell-group :title="cg.month + ' 充电' + cg.charges.length + '次'" v-for="cg of charges">
     <!-- {{charges && charges[0]}} -->
     <cell
-      v-for="(c, i) of charges" :title="c.address"
+      v-for="(c, i) of cg.charges" :title="c.address"
       :desc="`充入${c.charge_energy_added}kwh`"
-      :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} 行驶${(i > 0 ? (charges[i - 1].distance || 0) : 0).toFixed(0)}km`"
+      :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} ${c.distance && ('行驶' + Math.round(c.distance) + 'km') || ''}`"
     />
   </cell-group>
 </div>
@@ -20,5 +20,22 @@ import { duration } from '../filters'
 import api from '../api/vehicle'
 
 const charges = ref()
-api.getCharges().then(res => charges.value = res)
+api.getCharges().then(res => charges.value = res.reduceRight((arr, c, i) => {
+  if (i > 0) c.distance = res[i - 1].distance
+  else delete c.distance
+
+  const date = new Date(c.start_date_ts)
+  const month = date.getFullYear() + '/' + (date.getMonth() + 1)
+
+  if (arr.length > 0 && arr[0].month === month) {
+    arr[0].charges.unshift(c)
+  } else {
+    arr.unshift({
+      month,
+      charges: [c]
+    })
+  }
+
+  return arr
+}, []))
 </script>
