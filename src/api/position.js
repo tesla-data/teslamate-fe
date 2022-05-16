3da7d6509d42f78843e4ada7a6b8054ff872d9e0
Tal <talrasha007@gmail.com>
2022-05-16 23:08:35 +0800

    trip chart

--- src/api/position.js
+++ src/api/position.js
@@ -16,7 +16,7 @@ SELECT
 	convert_km(ideal_battery_range_km, '${length_unit}') AS "Range [${length_unit}]",
 	--convert_km(est_battery_range_km, '${length_unit}') AS "Range (est.) [${length_unit}]",
 	battery_level AS "SOC [%]",
-	usable_battery_level AS "usable SOC [%]",
+	usable_battery_level AS "Usable SOC [%]",
   convert_celsius(outside_temp, '${temp_unit}') AS "Outside Temperature [°${temp_unit}]",
 	convert_celsius(inside_temp, '${temp_unit}') AS "Inside Temperature [°${temp_unit}]",
 	convert_celsius(driver_temp_setting, '${temp_unit}') as "Driver Temperature [°${temp_unit}]",
@@ -30,6 +30,40 @@ WHERE
  $__timeFilter(date)
 ORDER BY
 	date ASC
+    `
+  }], from, to)
+}
+
+export function getPositionsBig(from, to, length_unit = 'km', temp_unit = 'C', alternative_length_unit = 'm') {
+  return query([{
+    refId: 'positions',
+    rawSql: `
+SELECT
+	$__timeGroup(date, '5s'),
+  ROUND(convert_m(avg(elevation), '${alternative_length_unit}')) AS "Elevation [${alternative_length_unit}]",
+  avg(latitude) AS latitude,
+  avg(longitude) AS longitude,
+	convert_km(avg(speed::numeric), '${length_unit}') AS "Speed [${length_unit}/h]",
+	avg(power) AS "Power [kW]",
+	avg(battery_heater::integer*100) AS "Preconditioning",
+	convert_km(avg(ideal_battery_range_km), '${length_unit}') AS "Range [${length_unit}]",
+	avg(battery_level) AS "SOC [%]",
+	avg(usable_battery_level) AS "Usable SOC [%]"
+  --convert_celsius(avg(outside_temp), '${temp_unit}') AS "Outside Temperature [°${temp_unit}]",
+	--convert_celsius(avg(inside_temp), '${temp_unit}') AS "Inside Temperature [°${temp_unit}]",
+	--convert_celsius(avg(driver_temp_setting), '${temp_unit}') as "Driver Temperature [°${temp_unit}]",
+	--convert_celsius(avg(passenger_temp_setting), '${temp_unit}') as "Passenger Temperature [°${temp_unit}]",
+  --avg(is_climate_on::integer),
+	--avg(fan_status)
+FROM
+	positions
+WHERE
+ car_id = ${currentVehicle.value.id} AND
+ $__timeFilter(date)
+GROUP BY
+ 1
+ORDER BY
+ 1 ASC
     `
   }], from, to)
 }
--- src/views/Trip.vue
+++ src/views/Trip.vue
@@ -1,6 +1,10 @@
 <template>
 <navbar @on-click-back="$router.go(-1)" :title="`${route.query.display}行程`" class="navbar" />
-<div class="page">
+  <div class="page">
+  <cell-group title="">
+    <div ref="driveChart" style="height: 300px" />
+    <div ref="elevationChart" style="height: 150px" />
+  </cell-group>
   <charge-cell-group :charges="tripStats.charges" />
   <drive-cell-group :drives="tripStats.drives" />
   <!-- <div v-if="tripStats.charges && tripStats.drives">
@@ -19,16 +23,28 @@ export default {
 <script setup>
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
-import { Navbar } from '@nutui/nutui'
+import { Navbar, CellGroup } from '@nutui/nutui'
 
 import { stats } from '../api/stats'
+import { getPositionsBig } from '../api/position'
+import { drawChart } from '../components/PositionChart'
 import DriveCellGroup from '../components/DriveCellGroup.vue'
 import ChargeCellGroup from '../components/ChargeCellGroup.vue'
 
 const tripStats = ref({})
 const route = useRoute()
-// const params = route.params
 
+const driveChart = ref()
+const elevationChart = ref()
 const { from, to } = route.query
-if (from && to ) stats(from, to).then(res => tripStats.value = res)
+if (from && to ) {
+  stats(from, to).then(res => tripStats.value = res)
+  getPositionsBig(from, to).then(positions => {
+    drawChart(positions, driveChart.value, '驾驶数据', [
+      'Range [km]',
+      'SOC [%]'
+    ])
+    drawChart(positions, elevationChart.value, '海拔', ['Elevation [m]'])
+  })
+}
 </script>
