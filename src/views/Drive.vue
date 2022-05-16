23fce1e9af2e5619c49d41dbcdf1dafcc970a05e
Tal <talrasha007@gmail.com>
2022-05-16 23:24:38 +0800

    drive chart

--- src/views/Drive.vue
+++ src/views/Drive.vue
@@ -16,7 +16,12 @@
       :sub-title="`${new Date(drive.end_date_ts).toLocaleString()} 温度${drive.outside_temp_c}℃`"
     />
   </cell-group>
-  <position-chart :from="from" :to="to" />
+  <cell-group title="">
+    <div ref="driveChart" style="height: 200px" />
+    <div ref="batteryChart" style="height: 150px" />
+    <div ref="elevationChart" style="height: 150px" />
+    <div ref="temperatureChart" style="height: 150px" />
+  </cell-group>
 </div>
 </template>
 
@@ -31,17 +36,34 @@ import { useRoute } from 'vue-router'
 import { ref } from 'vue'
 import { Navbar, CellGroup, Cell } from '@nutui/nutui'
 
+import { getPositions } from '../api/position'
 import { getDriveDetail } from '../api/drive'
-import PositionChart from '../components/PositionChart.vue'
+import { drawChart } from '../components/PositionChart'
 
 const route = useRoute()
 const drive = ref()
-const from = ref()
-const to = ref()
+const driveChart = ref()
+const batteryChart = ref()
+const elevationChart = ref()
+const temperatureChart = ref()
 getDriveDetail(route.query.drive_id).then(async res => {
   const { start_date_ts, end_date_ts } = drive.value = res[0]
-  from.value = start_date_ts
-  to.value = end_date_ts
-  // positions.value = await getPositions(start_date_ts, end_date_ts)
+  const positions = await getPositions(start_date_ts, end_date_ts)
+  drawChart(positions, driveChart.value, '驾驶数据', [
+    'Speed [km/h]',
+    'Power [kW]'
+  ])
+  drawChart(positions, batteryChart.value, '电池数据', [
+    'Range [km]',
+    // 'SOC [%]'
+  ])
+  drawChart(positions, elevationChart.value, '海拔', ['Elevation [m]'])
+  drawChart(positions, temperatureChart.value, '温度', [
+    'Outside Temperature [°C]',
+    'Inside Temperature [°C]',
+    'Driver Temperature [°C]',
+    'is_climate_on',
+    'fan_status'
+  ])
 })
 </script>
