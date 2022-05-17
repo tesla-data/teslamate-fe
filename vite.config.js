c00bf5f30c23e5e73ef11fc466d8858678fa9402
Tal <talrasha007@gmail.com>
2022-05-18 00:01:18 +0800

    dev serve on 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
   plugins: [
     vue(),
     styleImport({
