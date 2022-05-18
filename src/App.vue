2f6a0041b316332ec28bab4302cc2151f30b144b
Tal <talrasha007@gmail.com>
2022-05-18 15:34:23 +0800

    disable select

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,14 @@ html, body {
   margin: 0;
   padding: 0;
   background-color: #f7f8fa;
+  
+  -webkit-touch-callout:none;
+  -webkit-user-select:none;
+  -khtml-user-select:none;
+  -moz-user-select:none;
+  -ms-user-select:none;
+  user-select:none;
+  -webkit-tap-highlight-color:rgba(0,0,0,0);
 }
 
 .page {
