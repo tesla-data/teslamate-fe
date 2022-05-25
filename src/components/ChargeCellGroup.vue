3f36acefdc452d6ca1535809eb21c534a1fff580
Tal <talrasha007@gmail.com>
2022-05-25 22:22:43 +0800

    fix link param

--- src/components/ChargeCellGroup.vue
+++ src/components/ChargeCellGroup.vue
@@ -2,7 +2,7 @@
 <cell-group v-if="charges && charges.length > 0" :title="`${title || ''} 充电${charges.length}次 ${sum(charges)}度`" :desc="`${sumDc(charges, 'dc')}${sumDcTime(charges)} ${sumAc(charges, 'ac')}`">
   <cell
     v-for="(c, i) of charges" :title="c.address"
-    :to="{ name: 'Charge', query: { id: c.id, mode: c.mode, lat: c.latitude, lng: c.longitude, from: c.start_date_ts, to: c.end_date_ts }, params: c }"
+    :to="{ name: 'Charge', query: $route.query.hash ? { id: c.id, hash: $route.query.hash, href: 'Home' } : { id: c.id, mode: c.mode, lat: c.latitude, lng: c.longitude, from: c.start_date_ts, to: c.end_date_ts }, params: c }"
     :desc="`充入${c.charge_energy_added}kwh`"
     :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} ${c.distance && ('行驶' + Math.round(c.distance) + 'km') || ''}`"
     is-link
--- src/components/DriveCellGroup.vue
+++ src/components/DriveCellGroup.vue
@@ -10,7 +10,7 @@
   </template>
   <cell
     v-for="d of drives" :title="d.start_address"
-    :to="{ name: 'Drive', query: { drive_id: d.drive_id }, params: d }"
+    :to="{ name: 'Drive', query: $route.query.hash ? { id: d.drive_id, hash: $route.query.hash, href: 'Home' } : { drive_id: d.drive_id }, params: d }"
     :desc="`${d.duration_min}分钟 | ${d.distance_km.toFixed(0)}km`"
     :sub-title="`温度${d.outside_temp_c}℃ 能耗${d.consumption_kwh_km && d.consumption_kwh_km.toFixed(0)}Wh/km ${d.efficiency ? '' : '(*)'}`"
     is-link
