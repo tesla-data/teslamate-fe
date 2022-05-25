9a6f8c348e6d4bca854745dfe2166259a9cd4a40
Tal <talrasha007@gmail.com>
2022-05-26 00:25:47 +0800

    build multi page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { createStyleImportPlugin, NutuiResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        trip: resolve(__dirname, 'trip.html')
+      }
+    }
+  },
   server: {
     host: '0.0.0.0'
   },
