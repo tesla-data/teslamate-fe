c489097b780bf058b890c0cfcc0b55e935659400
Tal <talrasha007@gmail.com>
2022-05-26 17:51:10 +0800

    cdn

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { createStyleImportPlugin, NutuiResolve } from 'vite-plugin-style-import'
+import importToCDN, { autoComplete } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,34 @@ export default defineConfig({
       resolves: [
         NutuiResolve()
       ]
+    }),
+    importToCDN({
+      modules: [
+        autoComplete('vue'),
+        autoComplete('axios'),
+        autoComplete('lodash'),
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: 'dist/vue-router.global.min.js'
+        },
+        {
+          name: 'highcharts',
+          var: 'Highcharts',
+          path: 'highcharts.min.js'
+        },
+        {
+          name: 'leaflet',
+          var: 'leaflet',
+          path: 'dist/leaflet-src.min.js'
+        },
+        {
+          name: '@nutui/nutui',
+          var: 'nutui',
+          path: 'dist/nutui.umd.js',
+          css: 'dist/style.css'
+        }
+      ],
     })
   ],
   css: {
