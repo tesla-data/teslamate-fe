9d420e68b4174ed2cf17ac9e32fcc22447ea1a6e
Tal <talrasha007@gmail.com>
2022-10-06 20:54:15 +0800

    update drives data when vehicle change

--- src/views/Drives.vue
+++ src/views/Drives.vue
@@ -9,6 +9,7 @@
 import { ref, onActivated } from 'vue'
 import TopNav from '../components/TopNav.vue'
 
+import { currentVehicle } from '../settings'
 import DriveCellGroup from '../components/DriveCellGroup.vue'
 import { getDrives, groupDrives } from '../api/drive'
 
@@ -16,6 +17,8 @@ let drives = []
 const drivesGroups = ref([])
 
 onActivated(() => {
+  if (drives[0] && drives[0].car_id !== currentVehicle.value.id) drives = []
+
   const from = drives[0] && (drives[0].start_date_ts + 1000) || new Date().setHours(0, 0, 0, 0) - 30 * 86400 * 1000
   const to = Date.now()
   getDrives(from, to).then(res => {
