f905f33dc1f1ce8c9293fa76985dc0f3ee97e980
Tal <talrasha007@gmail.com>
2023-03-25 23:36:38 +0800

    100%

--- src/components/ChargeCellGroup.vue
+++ src/components/ChargeCellGroup.vue
@@ -5,7 +5,7 @@
     :class="{ full: c.end_battery_level === 100 }"
     :to="{ name: 'Charge', query: $route.query.hash ? { id: c.id, hash: $route.query.hash, href: 'Home' } : { id: c.id, car_id: c.car_id, mode: c.mode, lat: c.latitude, lng: c.longitude, from: c.start_date_ts, to: c.end_date_ts }, params: c }"
     :desc="c.charge_energy_added !== null ? `${c.mode === 'dc' ? '⚡' : ''}充入${c.charge_energy_added.toFixed(1)}kwh` : '进行中'"
-    :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} ${c.end_battery_level === 100 ? '(已充满)' : ''} ${c.distance && ('行驶' + Math.round(c.distance) + 'km') || ''}`"
+    :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} ${c.end_battery_level === 100 ? '(100%)' : ''} ${c.distance && ('行驶' + Math.round(c.distance) + 'km') || ''}`"
     is-link
   />
 </cell-group>
