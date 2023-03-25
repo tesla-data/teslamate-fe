5dcae0c89284e15d4908e56486ef3af8b5eee7fe
Tal <talrasha007@gmail.com>
2023-03-25 23:20:36 +0800

    charge 100% symbol

--- src/components/ChargeCellGroup.vue
+++ src/components/ChargeCellGroup.vue
@@ -3,7 +3,7 @@
   <cell center
     v-for="(c, i) of charges" :title="c.address"
     :to="{ name: 'Charge', query: $route.query.hash ? { id: c.id, hash: $route.query.hash, href: 'Home' } : { id: c.id, car_id: c.car_id, mode: c.mode, lat: c.latitude, lng: c.longitude, from: c.start_date_ts, to: c.end_date_ts }, params: c }"
-    :desc="c.charge_energy_added !== null ? `充入${c.charge_energy_added.toFixed(1)}kwh` : '进行中'"
+    :desc="c.charge_energy_added !== null ? `${c.end_battery_level === 100 ? '⚡' : ''}充入${c.charge_energy_added.toFixed(1)}kwh` : '进行中'"
     :sub-title="`${new Date(c.start_date_ts).toLocaleDateString()} 用时${duration(c.duration_min * 60 * 1000 || 0)} ${c.distance && ('行驶' + Math.round(c.distance) + 'km') || ''}`"
     is-link
   />
