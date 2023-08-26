55cf606c63320de8f612c612b49186c66c309ff4
talrasha007 <talrasha007@gmail.om>
2023-08-26 19:48:07 +0800

    test functions.

--- functions/j/[target].js
+++ functions/j/[target].js
@@ -1,6 +1,8 @@
 
-export async function onRequest({ params: target }) {
-  const resp = await fetch(`https://reurl.cc/${target}`)
+export async function onRequest(context) {
+  return new Response(context.params.target)
+
+  const resp = await fetch(`https://reurl.cc/${context.params.target}`)
   const reurl = resp.headers.get('target')
   return Response.redirect(reurl, 301)
 }
