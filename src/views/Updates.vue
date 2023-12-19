b6af0389d7bf939df65914b899389db8ef5d0124
talrasha007 <talrasha007@gmail.om>
2023-12-19 17:18:33 +0800

    fix update use time.

--- src/views/Updates.vue
+++ src/views/Updates.vue
@@ -6,7 +6,7 @@
     <cell
       v-for="(u, i) of updates" :title="u.version"
       :desc="`平均续航${(u.avg_ideal_range_km || 0).toFixed(0)}km`"
-      :sub-title="`${new Date(u.time).toLocaleDateString()} 使用${days(i > 0 ? updates[i - 1].since_last_update * 1000 : Date.now() - u.time )} 充电${u.chg_ct}次`"
+      :sub-title="`${new Date(u.time).toLocaleDateString()} 使用${days((i > 0 ? updates[i - 1].time : Date.now()) - u.time )} 充电${u.chg_ct}次`"
     />
   </cell-group>
 </div>
